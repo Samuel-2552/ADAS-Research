fac882b717558dc775b1432f636c5bde24c6e600
Satya <satya05112003@gmail.com>
2023-07-26 20:36:57 +0530

    fixed errors

--- distance-calc/base/main.py
+++ distance-calc/base/main.py
@@ -29,15 +29,12 @@ def distanceMts(focal_length, image_height_of_object_pxl, actual_height_of_objec
     return (actual_height_of_object_meters*focal_length)/image_height_of_object_pxl
 
 
-def calculate(img_src):
-    # Image
-    image=cv2.imread(img_src)
-
+def calculate(image):
     # Model
     model=YOLO("../yolov8l.pt")
 
     # Prediction
-    yolo_pred=model.predict(img_src)
+    yolo_pred=model.predict(image)
     tensor=yolo_pred[0].boxes.data
     names=yolo_pred[0].names
 
@@ -53,7 +50,7 @@ def calculate(img_src):
             dis=round(distanceMts(focal_length,y2-y1,objects_height[label]),2)
 
             cv2.rectangle(image,start_corner,end_corner,(0,255,0),2)
-            text_size=cv2.getTextSize(label,cv2.FONT_HERSHEY_COMPLEX,0.5,1)[0]
+            text_size=cv2.getTextSize(str(dis),cv2.FONT_HERSHEY_COMPLEX,0.5,1)[0]
             text_x=x1
             text_y=y1-5
             cv2.rectangle(image, (text_x, text_y), (text_x + text_size[0], text_y - text_size[1]), (0,255,0), -1)
@@ -62,19 +59,50 @@ def calculate(img_src):
 
     return image
 
+# Note- The sample input images from 12 to 21 are not calibrated for the primary camera as those images are randomly taken just for testing
 
-img_range_st=13
-img_range_end=22
+# Image Processing
+
+img_range_st=4
+img_range_end=5
 
 for i in range(img_range_st,img_range_end):
-    img_src="../images/"+str(i)+".jpg"
-    img_dst="../output_images/"+str(i)+".jpg"
-    image=calculate(img_src)
+    img_src="../input/images/"+str(i)+".jpg"
+    img_dst="../output/images/"+str(i)+".jpg"
+
+    image=cv2.imread(img_src)
+
+    image=calculate(image)
     cv2.imwrite(img_dst,image)
 
 # plt.figure(1)
 # plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 # plt.show()
+
+
+# Video Processing
+
+# vid_src="../input/videos/vidtest.mp4" # 0 for default camera
+# video_capture=cv2.VideoCapture(vid_src)
+# frame_width = int(video_capture.get(3))
+# frame_height = int(video_capture.get(4))
+# fps = video_capture.get(cv2.CAP_PROP_FPS)
+
+# output_path = '../output/videos/out_vidtest.avi'
+# output_video = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'XVID'), fps, (frame_width, frame_height))
+
+# while True:
+#     ret,frame=video_capture.read()
+#     if not ret:
+#         break
+#     output=calculate(frame)
+#     output_video.write(output)
+
+# video_capture.release()
+# output_video.release()
+
+
+# End
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
---
+++ distance-calc/readme.md
@@ -0,0 +1,2 @@
+# ADAS Distance Calculation
+
